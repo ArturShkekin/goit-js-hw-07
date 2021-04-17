c7798b1218ac4beaec6e3ec6fce7ac75dac8397b
Artur Shkekin <64356091+ArturShkekin@users.noreply.github.com>
2021-04-17 16:10:06 +0300

    Сделал 1 задачу

---
+++ js/task-01.js
@@ -0,0 +1,20 @@
+const navitEl = document.querySelectorAll(".item").length;
+console.log(`В списке ${navitEl} категории`);
+
+const nameCat1 = document.querySelector(".сategory1");
+console.log("Категория:",nameCat1.textContent);
+
+const list1El = document.querySelector(".item-сategory1");
+console.log(`Количество элементов ${list1El.children.length}`);
+
+const nameCat2 = document.querySelector(".сategory2");
+console.log("Категория:", nameCat2.textContent);
+
+const list2El = document.querySelector(".item-сategory2");
+console.log(`Количество элементов ${list2El.children.length}`);
+
+const nameCat3 = document.querySelector(".сategory3");
+console.log("Категория:", nameCat3.textContent);
+
+const list3El = document.querySelector(".item-сategory3");
+console.log(`Количество элементов ${list3El.children.length}`);
